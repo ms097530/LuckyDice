ab9512db882a86bb1137114334cb247608a66933
Michael Schultz <mikewschultz9029@gmail.com>
2023-05-16 14:18:54 -0400

    added win condition

--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -6,9 +6,11 @@ import './LuckyN.css'
 export default function LuckyN({ numDice = 2, goal = 7 })
 {
     const [dice, setDice] = useState(() => getRolls(numDice))
+    const isWinner = sum(dice) === goal
 
     return (
         <section className="LuckyN">
+            <h1>Lucky{goal} {isWinner && "You win!"}</h1>
             <Dice dice={dice} />
         </section>
     )
